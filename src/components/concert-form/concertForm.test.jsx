cbfa77f93ad385a65e9ca91aee31714a4f00459c
jzilg <jzg@dasburo.com>
2019-07-25 17:11:13 +0200

    add first test for concertForm

---
+++ src/components/concert-form/concertForm.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+import ConcertForm from './ConcertForm'
+import concerts from '../../mock-data/concerts'
+
+describe('Loading', () => {
+    const goToConcerts = () => {}
+    const saveConcert = () => {}
+
+    it('should render correctly', () => {
+        const component = shallow((
+            <ConcertForm
+                concert={concerts[0]}
+                goToConcerts={goToConcerts}
+                saveConcert={saveConcert}
+            />
+        ))
+        const tree = shallowToJson(component)
+        expect(tree).toMatchSnapshot()
+    })
+})
