77c78cf2846d0d920e0e76fd38c7bba862267815
jzilg <jzg@dasburo.com>
2020-04-16 16:42:39 +0200

    update apiMiddleware

--- src/redux/actions/core/api.actions.ts
+++ src/redux/actions/core/api.actions.ts
@@ -4,6 +4,6 @@ import Error from '../../../entities/Error'
 
 export const apiRequest = createAction('API | EVENT | API_REQUEST')<ApiRequestOptions, { causedBy: string }>()
 
-export const apiSuccess = createAction('API | EVENT | API_SUCCESS')<{ successAction: Function; data: object }, { causedBy: string }>()
+export const apiSuccess = createAction('API | EVENT | API_SUCCESS')<{ successAction: Function; data: unknown }, { causedBy: string }>()
 
 export const apiFailure = createAction('API | EVENT | API_ERROR')<{ failureAction: Function; error: Error }, { causedBy: string }>()
