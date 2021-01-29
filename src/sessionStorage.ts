080511a412fd8a11cc2a0fce5c1b8b988f0f1083
jzilg <jzg@dasburo.com>
2021-01-29 23:49:31 +0100

    improve type

--- src/sessionStorage.ts
+++ src/sessionStorage.ts
@@ -6,7 +6,7 @@ export function setStorageData(id: string, data: unknown): void {
 
 // must return undefined instead of null
 // otherwise the redux store throws an error on init
-export function getStorageData(id: string): unknown {
+export function getStorageData(id: string): unknown | undefined {
     const persistedData = STORAGE.getItem(id)
 
     if (persistedData !== null) {
