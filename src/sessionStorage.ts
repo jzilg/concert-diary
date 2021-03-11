d4857b41587de89d87b48ec05803456ceba6893d
jzilg <jzg@dasburo.com>
2021-03-11 11:21:50 +0100

    refactor function for readablilty

--- src/sessionStorage.ts
+++ src/sessionStorage.ts
@@ -4,14 +4,14 @@ export function setStorageData(id: string, data: unknown): void {
     STORAGE.setItem(id, JSON.stringify(data))
 }
 
-// must return undefined instead of null
-// otherwise the redux store throws an error on init
 export function getStorageData(id: string): unknown | undefined {
     const persistedData = STORAGE.getItem(id)
 
-    if (persistedData !== null) {
-        return JSON.parse(persistedData)
+    // must return undefined instead of null
+    // otherwise the redux store throws an error on init
+    if (persistedData === null) {
+        return undefined
     }
 
-    return undefined
+    return JSON.parse(persistedData)
 }
